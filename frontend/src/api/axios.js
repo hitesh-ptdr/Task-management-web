53d38a7146a0beae7e44ba42f1a5d1bd7a086133
hitesh-ptdr <hiteshp11062005@gmail.com>
2026-05-05 19:32:59 +0530

    fixed auth headers

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,3 @@
-
-
-// axios.js
 import axios from "axios";
 
 const instance = axios.create({
@@ -10,20 +7,17 @@ const instance = axios.create({
   },
 });
 
-// ✅ Request interceptor
 instance.interceptors.request.use(
   (config) => {
-    // Agar explicitly header set ho gaya ho, use karo
-    if (!config.headers.Authorization) {
-      // Admin token first
-      const adminToken = localStorage.getItem("token");
-      const devToken = localStorage.getItem("devToken");
+    const adminToken = localStorage.getItem("token");
+    const devToken = localStorage.getItem("devToken");
 
-      if (adminToken && config.url.includes("/admin")) {
-        config.headers.Authorization = `Bearer ${adminToken}`;
-      } else if (devToken && config.url.includes("/developers")) {
-        config.headers.Authorization = `Bearer ${devToken}`;
-      }
+    if (adminToken) {
+      config.headers.Authorization = `Bearer ${adminToken}`;
+    }
+
+    if (devToken) {
+      config.headers.Authorization = `Bearer ${devToken}`;
     }
 
     return config;
@@ -32,4 +26,3 @@ instance.interceptors.request.use(
 );
 
 export default instance;
-
